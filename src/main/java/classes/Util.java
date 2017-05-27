fef2a47f6270419d951c9a020f17a77a243c793a
Hp_Workplace <farkas0528@gmail.com>
2017-05-27 20:53:08 +0200

    #3

--- src/main/java/classes/Util.java
+++ src/main/java/classes/Util.java
@@ -1,12 +1,24 @@
 package classes;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Created by Hp_Workplace on 2017. 05. 27..
  */
 public class Util {
 
-    public void printYoutubeJSON(String path){
+    public void printYoutubeJSON(String path) throws IOException {
 
+        byte[] jsonData = Files.readAllBytes(Paths.get(path));
+        ObjectMapper objectMapper = new ObjectMapper();
+        YoutubePojo youtubePojo = objectMapper.readValue(jsonData, YoutubePojo.class);
+
+        System.out.println(youtubePojo.toString());
     }
 
     public void convertYoutubeJSONtoXML(String source, String path){
