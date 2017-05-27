693806b553e96eea597f179eb2d1412ab8d59ce3
Hp_Workplace <farkas0528@gmail.com>
2017-05-27 17:00:12 +0200

    #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
  * Created by Hp_Workplace on 2017. 05. 27..
  */
 public class Main {
+
     public static void main(String[] args) throws IOException {
 
     }
--- src/main/java/Util.java
+++ src/main/java/Util.java
@@ -2,4 +2,12 @@
  * Created by Hp_Workplace on 2017. 05. 27..
  */
 public class Util {
+
+    public void printYoutubeJSON(String path){
+
+    }
+
+    public void convertYoutubeJSONtoXML(String source, String path){
+
+    }
 }
