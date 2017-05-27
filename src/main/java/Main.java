54c4dd4bf7313968f1b0a1526d5e70890d8a1f5f
Hp_Workplace <farkas0528@gmail.com>
2017-05-27 16:56:50 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+/**
+ * Created by Hp_Workplace on 2017. 05. 27..
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
---
+++ src/main/java/Util.java
@@ -0,0 +1,5 @@
+/**
+ * Created by Hp_Workplace on 2017. 05. 27..
+ */
+public class Util {
+}
